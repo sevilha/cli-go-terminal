c1905857e337f7e9650b03ee9e048b495a87cbd9
Vinicius Sevilha <vd.sevilha@gmail.com>
2022-03-02 21:38:21 -0300

    Feat: adding options of CLI

--- cmd/main.go
+++ cmd/main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"time"
 
 	"github.com/AlecAivazis/survey/v2"
-	"github.com/joho/godotenv"
 )
 
-var commands = []string{"build", "deploy", "test", "exit"}
+var commands = []string{"build", "deploy", "tests", "exit"}
 
 func build() {
-	err := godotenv.Load(".env")
+	out, err := exec.Command("go", "build", "../pkg/main.go").Output()
+
 	if err != nil {
-		log.Fatal("Error to load .env file")
+		log.Fatal(err)
 	}
+	fmt.Println(string(out))
 }
 
 func deploy() {
@@ -27,7 +29,7 @@ func deploy() {
 func test() {
 	option := ""
 	prompt := &survey.Select{
-		Message: "Select a command:",
+		Message: "Select a test command:",
 		Options: []string{"test-all", "test-file"},
 	}
 
@@ -51,7 +53,45 @@ func testAll() {
 }
 
 func testFile() {
+	files, err := IOReadDir("./test")
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file := ""
+	prompt := &survey.Select{
+		Message: "Select file to test:",
+		Options: files,
+	}
+
+	survey.AskOne(prompt, &file)
+
+	if &file == nil {
+		log.Fatal("File not selected")
+	}
+
+	out, err := exec.Command("go", "test", "-v", "./test/"+file).Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(out))
+}
+
+func IOReadDir(dir string) ([]string, error) {
+	var files []string
+	fileInfo, err := ioutil.ReadDir("./test/")
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range fileInfo {
+		files = append(files, file.Name())
+	}
+
+	return files, nil
 }
 
 func exit() {
@@ -77,7 +117,7 @@ func main() {
 		build()
 	case "deploy":
 		deploy()
-	case "test":
+	case "tests":
 		test()
 	case "exit":
 		exit()
